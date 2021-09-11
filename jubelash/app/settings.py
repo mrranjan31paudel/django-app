2a9df46fb3e9678213239a78bcf39a4e43922719
Ranjan Paudel <paudeljimrranjan@gmail.com>
2021-09-11 23:17:02 +0545

    Add some settings for statisticfiles

--- jubelash/app/settings.py
+++ jubelash/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from dotenv import dotenv_values
+from os import path as os_path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os_path.realpath(BASE_DIR).replace('/jubelash', '/staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
